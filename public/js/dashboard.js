4ef5da49555622f53a2bdefb1880a5331ae4fec2
delynko <delynko@gmail.com>
2019-05-04 12:36:51 -0600

    create containers for charts. install chart.js and moment dependency

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,20 +1,22 @@
-fetch('/data')
-    .then((res) => {
-        return res.json();
-    })
-    .then((result) => {
-        let tripDuration = 0;
-        let lightDuration = 0;
-        let tripData = JSON.parse(result[0]);
-        let lightData = JSON.parse(result[1]);
-        tripData.forEach((x) => {
-            tripDuration += x.duration;
-        });
-        lightData.forEach((d) => {
-            lightDuration += d.duration;
-        });
-        $('#data').html(`Total number of trips: ${tripData.length}<br>
-                        Total time driving: ${(tripDuration / 60).toFixed(2)} minutes<br>
-                        Total number of red lights stopped at: ${lightData.length}<br>
-                        Time spent stopped at red lights: ${(lightDuration / 60).toFixed(2)} minutes`)
-    });
+// fetch('/data')
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((result) => {
+//         let tripDuration = 0;
+//         let lightDuration = 0;
+//         let tripData = JSON.parse(result[0]);
+//         let lightData = JSON.parse(result[1]);
+//         tripData.forEach((x) => {
+//             tripDuration += x.duration;
+//         });
+//         lightData.forEach((d) => {
+//             lightDuration += d.duration;
+//         });
+//         $('#data').html(`Total number of trips: ${tripData.length}<br>
+//                         Total time driving: ${(tripDuration / 60).toFixed(2)} minutes<br>
+//                         Total number of red lights stopped at: ${lightData.length}<br>
+//                         Time spent stopped at red lights: ${(lightDuration / 60).toFixed(2)} minutes`)
+//     });
+
+console.log(moment().format('MMMM Do YYYY'))
